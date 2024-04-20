0636a3988ae83022f4c89e579d50e4c8483ef54e
NormalHub <143687048+NormalHub@users.noreply.github.com>
2024-04-20 19:44:28 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,12 +6,10 @@ import {convertToHTML} from "./markdown.js";
 
 //重复调用的函数
 function getUserName(){
-  for(var t=cookies.length;t--;t>0){
-    if(cookies[t].indexOf("user_name")!=-1){var name = cookies[t].split("=")[1];}
-  }
+  for(var t=cookies.length;t--;t>0){if(cookies[t].indexOf("user_name")!=-1){var name = cookies[t].split("=")[1];}}
   var notNull = /[^ ]/g;
   if(name==""||name==null||!notNull.test(name)||name=="null"){
-    var name = prompt("请输入用户名","游客"+Math.floor(Math.random()*5000));
+    var name = prompt("Please enter your user name","guest"+Math.floor(Math.random()*5000));
     var date = new Date();
     date.setTime(date.getTime()+(365*24*3600000));
     var expires="expires="+date.toGMTString()+";";
@@ -30,57 +28,52 @@ function alertTip(text){
 }
 
 var cookies = decodeURI(document.cookie).split("; ");
-//获取内容
-function getOpus(){
-  var opudiv=document.getElementsByClassName("opu")[0];
-  var getOpus=new XMLHttpRequest();
-  getOpus.open("GET", "./document/wokes", true);
-  getOpus.onreadystatechange = function(){
-    if(getOpus.readyState==4 && getOpus.status==200){
-      var opus = getOpus.responseText.split("\n");
-      for(var times=opus.length;times--;times>0){
-        var opusInformation = opus[times].split("&&");
-        var type=opusInformation[0].split("=")[1];
-        var opusName=opusInformation[1].split("=")[1];
-        var opusUrl=opusInformation[2].slice(opusInformation[2].indexOf("=")+1);
-        var opusIntroduction=opusInformation[3].split("=")[1];
-        var time=opusInformation[4].split("=")[1];
-        wokes(type,opusName,opusUrl,opusIntroduction,time);
-      }
+//获取视频
+function getvideo(){
+  var videodiv=document.getElementsByClassName("video")[0];
+  var getvideo=new XMLHttpRequest();
+  getvideo.open("GET", "./document/wokes", true);
+  getvideo.onreadystatechange = function(){
+  if(getvideo.readyState==4 && getvideo.status==200){
+    var video = getvideo.responseText.split("\n");
+    for(var times=video.length;times--;times>0){
+      var videoInformation = video[times].split("&&");
+      var type=videoInformation[0].split("=")[1];
+      var videoName=videoInformation[1].split("=")[1];
+      var videoUrl=videoInformation[2].slice(videoInformation[2].indexOf("=")+1);
+      var videoIntroduction=videoInformation[3].split("=")[1];
+      var time=videoInformation[4].split("=")[1];
+      wokes(type,videoName,videoUrl,videoIntroduction,time);
     }
+  }
   };
-  getOpus.send();
-  function wokes(type,opusName,opusUrl,opusIntroduction,time){
+  getvideo.send();
+  function wokes(type,videoName,videoUrl,videoIntroduction,time){
     var box = document.createElement("a");
     box.classList.add("box");
     var title = document.createElement("div");
     title.classList.add("title");
     var timeElement = document.createElement("div");
-    timeElement.innerHTML="上传时间："+time;
-    title.innerHTML=opusName;
+    timeElement.innerHTML="time: "+time;
+    title.innerHTML=videoName;
     if(type=="视"){
-      box.href = "./video/?url="+opusUrl+"&&isShare=false";
+      box.href = "./video/?url="+videoUrl+"&&isShare=false";
     }else if(type=="文"){
-      box.href = opusUrl;
+      box.href = videoUrl;
     }else if(type.indexOf("share")!=-1){
-      box.href = "./video/?url="+opusUrl+"&&isShare=true";
+      box.href = "./video/?url="+videoUrl+"&&isShare=true";
     }
     box.appendChild(title);
     box.appendChild(timeElement);
-    opudiv.appendChild(box);
+    videodiv.appendChild(box);
   }
 }
-getOpus();
+getvideo();
 //获取博客
 function getBlog(){
   var xmlhttp;
   if(window.XMLHttpRequest){xmlhttp = new XMLHttpRequest();}else{xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");}
-  xmlhttp.onreadystatechange = function(){
-    if (xmlhttp.readyState==4 && xmlhttp.status==200){
-      var list = JSON.parse(this.responseText);
-      for(var x in list){creatBlog(list[x].title,list[x].writer,list[x].url,list[x].time);}
-    }
-  }
+  xmlhttp.onreadystatechange = function(){if(xmlhttp.readyState==4 && xmlhttp.status==200){var list = JSON.parse(this.responseText);for(var x in list){creatBlog(list[x].title,list[x].writer,list[x].url,list[x].time);}}}
   xmlhttp.open("GET","./document/blog.json",true);
   xmlhttp.setRequestHeader("Content-Type","application/json;charset=UTF-8");
   xmlhttp.send();
@@ -90,8 +83,8 @@ function getBlog(){
     var titleElement = document.createElement("div");
     titleElement.classList.add("title");
     var timeElement = document.createElement("div");
-    timeElement.innerHTML="时间："+time;
-    titleElement.innerHTML="标题："+title;
+    timeElement.innerHTML="time: "+time;
+    titleElement.innerHTML="title: "+title;
     box.href = url;
     box.appendChild(titleElement);
     box.appendChild(timeElement);
@@ -104,12 +97,8 @@ async function getPostings(){
   var postings = document.getElementById("postings");
   const { data, error } = await supabase.from('posting').select();
   postings.innerHTML="";
-  function dateData(property,bol) { //property是排序传入的key,bol为true升序false为降序
-    return function(a,b){
-      var value1 = a[property];
-      var value2 = b[property];
-      if(bol){return Date.parse(value1)-Date.parse(value2);}else{return Date.parse(value2)-Date.parse(value1)}
-    } 
+  function dateData(property,bol){//property是排序传入的key,bol为true升序false为降序
+    return function(a,b){var value1 = a[property];var value2 = b[property];if(bol){return Date.parse(value1)-Date.parse(value2);}else{return Date.parse(value2)-Date.parse(value1)}} 
   } 
   data.sort(dateData("like", true));
   var postingLiked="";
@@ -160,26 +149,29 @@ async function getPostings(){
     if(postingLiked.indexOf(data[i].time.toString())!=-1){document.getElementsByClassName("fa-thumbs-o-up")[document.getElementsByClassName("fa-thumbs-o-up").length-1].className="fa fa-thumbs-up";}
   }
 }
+getPostings();
+// 详情
 var postMessageDom = document.getElementById("postingsMessage");
-var postBg = document.getElementById("postBg");
-postBg.style.display = "none";
-postBg.onmousedown = function(){
-  if(event.target != postBg){return;}
-  postBg.onmouseup = function(){
-    postBg.onmouseup = null;
-    if(event.target != postBg){return;}
-    postBg.style.display = postBg.style.display=="none" ? "flex" : "none";
-  }
-}
 async function postingsMessage(time,content,date,sender){
-  postBg.style.display = "flex";
+  if(window.innerWidth<=600){//小窗口/移动端
+    document.getElementById("postBg").style.height = "80%";
+    var bg = document.createElement("div");
+    setTimeout(function(){bg.className = "bg";},200);
+    document.body.insertBefore(bg,document.body.children[0]);
+    bg.onclick = function(){
+      this.remove();
+      document.getElementById("postBg").style.height = 0;
+      bg.onclick = null;
+    }
+  }
+  
   document.getElementsByClassName("discuss").id = time;
-  postMessageDom.innerHTML = '<p><i class="fa fa-spinner fa-pulse"></i>加载中</p>';
+  postMessageDom.innerHTML = '<p><i class="fa fa-spinner fa-pulse"></i>loading...</p>';
   const { data, error } = await supabase.from('posting').select();
   postMessageDom.innerHTML = "";
   
   var element = document.createElement("div");
-  element.innerHTML = '<p id="description">'+sender+'：</p><p id="content">'+content+'</p>'+date+"<p>回复</p>";
+  element.innerHTML = '<p id="description">'+sender+'：</p><p>'+content+'</p>'+date+"<p>reply:</p>";
   postMessageDom.insertBefore(element,postMessageDom.children[0]);
   
   for(var t=data.length;t--;t>0){
@@ -190,31 +182,29 @@ async function postingsMessage(time,content,date,sender){
     }
   }
 }
-getPostings();
 //发帖
-var publishingPostBg = document.getElementsByClassName("publishingPostBg")[0];
-var publishingPostElement = document.getElementsByClassName("publishingPost")[0];
-document.getElementsByClassName("postingsButton")[0].onclick = function(){publishingPostBg.style.display = "flex";}
-publishingPostBg.onmousedown = function(){
-  if(event.target != publishingPostBg){return;}
-  publishingPostBg.onmouseup = function(){
-    publishingPostBg.onmouseup = null;
-    if(event.target != publishingPostBg){return;}
-    publishingPostBg.style.display = "none";
+document.getElementsByClassName("postButton")[0].onclick = function(){
+  document.getElementsByClassName("publishingPost")[0].style.height = "80%";
+  var bg = document.createElement("div");
+  setTimeout(function(){bg.className = "bg";},200);
+  document.body.insertBefore(bg,document.body.children[0]);
+  bg.onclick = function(){
+    this.remove();
+    document.getElementsByClassName("publishingPost")[0].style.height = 0;
+    bg.onclick = null;
   }
 }
 document.getElementById("send").onclick = async function send(){
   var user = getUserName();
-  var content = document.getElementById("content").value;
+  var content = document.getElementById("postContent").value;
   var notNull = /[^ ]/g;
-  if(content==""||!notNull.test(content)){alertTip("帖子不能为空");return;}
+  if(content==""||!notNull.test(content)){alertTip("cannot be null");return;}
   var time = new Date().getTime();
-  alertTip("正在发往服务器");
   const { error } = await supabase.from('posting').insert({sender:user, content:content, time:time});
-  alertTip("完成！");
+  alertTip("the posting get sent");
   location.reload();
 }
-var postContent = document.getElementById('content');
+var postContent = document.getElementById('postContent');
 var addDoms = document.getElementsByClassName("add");
 addDoms[0].onclick = function(){
   var selectedText = postContent.value.substring(postContent.selectionStart, postContent.selectionEnd);
@@ -226,39 +216,25 @@ addDoms[1].onclick = function(){
 }
 addDoms[2].onclick = function(){
   var selectedText = postContent.value.substring(postContent.selectionStart, postContent.selectionEnd);
-  var link = prompt('请输入链接地址：');
+  var link = prompt('Please enter jump link: ');
   postContent.value = postContent.value.substring(0, postContent.selectionStart) + `[${selectedText}](${link})` + postContent.value.substring(postContent.selectionEnd);
 }
-const text = `
-# 标题<br>
-## 副标题<br>
-**粗体**<br>
-*斜体*<br>
-- 列表项<br>
-[一条链接](//kingdinner.top)<br>
-\`\`\`console.log('这是一个代码块')\`\`\`<br>
-这是一个普通段落。`;
-document.getElementsByClassName("example")[0].innerHTML = text;
-document.getElementsByClassName("example")[1].innerHTML = convertToHTML(text);
 //评论
 document.getElementsByClassName("discuss")[0].children[1].onclick = async function(){
   var user = getUserName();
   var content = document.getElementsByClassName("discuss")[0].children[0].value;
   var notNull = /[^ ]/g;
   if(content==""||!notNull.test(content)){
-    alertTip("帖子不能为空");
+    alertTip("cannot be null");
     return;
   }
   var follow_time = document.getElementsByClassName("discuss").id;
   var time = new Date().getTime()+"follow"+follow_time;
   document.getElementsByClassName("discuss")[0].children[0].value="";
-  setTimeout(function(){
-    alertTip("完成！")
-    var element = document.createElement("div");
-    element.innerHTML = "<p>"+user+"："+content+"</p>";
-    postMessageDom.appendChild(element);
-  },1000)
   const {error} = await supabase.from('posting').insert({content:content, time:time,sender:user});
+  var element = document.createElement("div");
+  element.innerHTML = "<p>"+user+"："+content+"</p>";
+  postMessageDom.appendChild(element);
 }
 //搜素
 document.getElementById("search").onclick = search;
@@ -271,63 +247,32 @@ function search(){
   a.href = "./search.html?"+key;
   a.click();
 }
-window.onkeydown = function (){
+window.addEventListener("keydown",function(){
   var key = event.key;
   var element = document.activeElement;
-  if(element.tagName == "INPUT"&&key == "Enter"){
-    search();
-    return;
-  }
-  if(element.parentElement.className == "discuss"&&key == "Enter"){
-    document.getElementsByClassName("discuss")[0].children[1].click();
-    return;
-  }
-}
+  if(element.tagName == "INPUT"&&key == "Enter"){search();return;}
+  if(element.parentElement.className == "discuss"&&key == "Enter"){document.getElementsByClassName("discuss")[0].children[1].click();return;}
+});
 //切换页面
-function turnPage(){
-  function saveCookie(page){
-    var date = new Date();
-    date.setTime(date.getTime()+(365*24*60*60000));
-    var expires="expires="+date.toGMTString()+";";
-    var content="page="+page+";";
-    document.cookie=content+expires+"path=/";
-  }
-  var tblElement = document.getElementsByClassName("tbl");
-  var postings = document.getElementsByClassName("postings")[0];
-  var opu = document.getElementsByClassName("opu")[0];
-  for(var t=tblElement.length;t--;t>0){
-    tblElement[t].addEventListener("click",function(){
-      if(this.id == "shipin"){
-        postings.style.width = 0;
-        postings.style.height = 0;
-        opu.style.width = "100%";
-        opu.style.height = "100%";
-        saveCookie("视频");
-      }else if(this.id == "tiezi"){
-        postings.style.width = "100%";
-        postings.style.height ="100%";
-        saveCookie("帖子");
-      }else if(this.id == "boke"){
-        postings.style.width = 0;
-        postings.style.height = 0;
-        opu.style.width = 0;
-        opu.style.height = 0;
-        saveCookie("博客");
-      }
-    })
-  }
-  for(var l=cookies.length;l--;l>0){
-    if(cookies[l].indexOf("page=视频")!=-1){
-      postings.style.width = 0;
-      postings.style.height = 0;
-    }else if(cookies[l].indexOf("page=博客")!=-1){
-      postings.style.width = 0;
-      postings.style.height = 0;
-      opu.style.width = 0;
-      opu.style.height = 0;
+(function(){
+  function saveCookie(page){var d=new Date();d.setTime(d.getTime()+(2592000000));var expires="expires="+d.toGMTString()+";";var content="page="+page+";";document.cookie=content+expires+"path=/";}
+  var tbl= document.getElementsByClassName("tbl");
+  var juzhong = document.getElementsByClassName("juzhong")[0];
+  var p = [document.getElementsByClassName("postings")[0],document.getElementsByClassName("blog")[0],document.getElementsByClassName("video")[0]];
+  for(var t=tbl.length;t--;t>0){tbl[t].addEventListener("click",function(){
+    for(var x in p){p[x].style.display="none";}
+    if(this.innerHTML=="posting"){
+      p[0].style.display="flex";
+      saveCookie("posting");
+    }else if(this.innerHTML=="blog"){
+      p[1].style.display="block";
+      saveCookie("blog");
+    }else if(this.innerHTML=="video"){
+      p[2].style.display="block";
+      saveCookie("video");
     }
-  }
-}
-turnPage();
-
+  })}
+  for(var t in cookies){if(cookies[t]=="page=posting"){p[0].style.display="flex";}else if(cookies[t]=="page=blog"){p[1].style.display="block";}else if(cookies[t]=="page=video"){p[2].style.display="block";}else{p[0].style.display="flex";}}
+  document.getElementsByClassName("frontDiv")[0].remove();
+}())
 //fetch('https://ipapi.co/json/').then(response=>response.json()).then(data=>{region = data.region;})
